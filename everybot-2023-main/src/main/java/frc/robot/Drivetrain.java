e80130ba26e09863993fd1a45dc36fbead5f2ebb
akugoyal <63987908+akugoyal@users.noreply.github.com>
2023-09-02 11:35:35 -0700

    Update Drivetrain.java

--- everybot-2023-main/src/main/java/frc/robot/Drivetrain.java
+++ everybot-2023-main/src/main/java/frc/robot/Drivetrain.java
@@ -8,5 +8,46 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drivetrain {
-    
+    CANSparkMax driveLeftSpark = new CANSparkMax(7, MotorType.kBrushed);
+    CANSparkMax driveRightSpark = new CANSparkMax(5, MotorType.kBrushed);
+    CANSparkMax driveLeftSparkTwo = new CANSparkMax(6, MotorType.kBrushed);
+    CANSparkMax driveRightSparkTwo = new CANSparkMax(8, MotorType.kBrushed);
+    CANcoder leftCoder = new CANcoder(35, "rio");
+    static Speed dtSpeed = new Speed();
+    static boolean changeSpeed = false;
+
+    /**
+   * Calculate and set the power to apply to the left and right
+   * drive motors.
+   * 
+   * @param turn Desired forward speed. Positive is forward.
+   * @param forward    Desired turning speed. Positive is counter clockwise from
+   *                above.
+   */
+  public void setDriveMotors(double turn, double forward) {
+    if (turn == 0 && forward == 0) {
+      driveLeftSpark.setIdleMode(IdleMode.kBrake);
+      driveLeftSparkTwo.setIdleMode(IdleMode.kBrake);
+      driveRightSpark.setIdleMode(IdleMode.kBrake);
+      driveRightSparkTwo.setIdleMode(IdleMode.kBrake);
+    }
+    SmartDashboard.putNumber("drive forward power (%)", forward);
+    SmartDashboard.putNumber("drive turn power (%)", turn);
+
+    /*
+     * positive turn = counter clockwise, so the left side goes backwards
+     */
+    double left = turn - forward;
+    double right = turn + forward;
+
+    SmartDashboard.putNumber("drive left power (%)", left);
+    SmartDashboard.putNumber("drive right power (%)", -right);
+
+    // see note above in robotInit about commenting these out one by one to set
+    // directions.
+    driveLeftSpark.set(left);
+    driveLeftSparkTwo.set(left);
+    driveRightSpark.set(right);
+    driveRightSparkTwo.set(right);
+}
 }
