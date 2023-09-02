684b387e687ccd9783518e95d9f123cf874a8d9e
akugoyal <63987908+akugoyal@users.noreply.github.com>
2023-09-01 21:12:57 -0700

    Drivetrain

---
+++ everybot-2023-main/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,12 @@
+package frc.robot;
+import com.ctre.phoenix6.hardware.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Drivetrain {
+    
+}
