0656b0aa3ec41ca5c9e23846c2798c2802145a6a
akugoyal <63987908+akugoyal@users.noreply.github.com>
2024-06-24 13:34:46 -0700

    Remove PivotToAngle.java

--- everybot-2023-main/src/main/java/frc/robot/commands/pivot/PivotToAngle.java
+++ everybot-2023-main/src/main/java/frc/robot/commands/pivot/PivotToAngle.java
@@ -5,40 +5,40 @@ import frc.robot.RobotMap;
 import frc.robot.subsystems.Arm;
 import frc.robot.util.MathUtil;
 
-public class PivotToAngle extends Command {
-    private RobotMap.Arm.Goal setpoint;
-    private double ref;
+// public class PivotToAngle extends Command {
+//     private RobotMap.Arm.Goal setpoint;
+//     private double ref;
 
-    public PivotToAngle(RobotMap.Arm.Goal goal) {
-        setpoint = goal;
-        addRequirements(Arm.getInstance());
-    }
+//     public PivotToAngle(RobotMap.Arm.Goal goal) {
+//         setpoint = goal;
+//         addRequirements(Arm.getInstance());
+//     }
 
-    public void execute() {
-        switch (setpoint) {
-            case ZERO:
-                ref = 0;
-                break;
-            case SETPOINT1:
-                ref = 30.0;
-                break;
-            case SETPOINT2:
-                ref = 60.0;
-                break;
-            case SETPOINT3:
-                ref = 90.0;
-                break;
-        }
-        Arm.getInstance().moveToPosition(ref);
+//     public void execute() {
+//         switch (setpoint) {
+//             case ZERO:
+//                 ref = 0;
+//                 break;
+//             case SETPOINT1:
+//                 ref = 30.0;
+//                 break;
+//             case SETPOINT2:
+//                 ref = 60.0;
+//                 break;
+//             case SETPOINT3:
+//                 ref = 90.0;
+//                 break;
+//         }
+//         Arm.getInstance().moveToPosition(ref);
 
-    }
+//     }
 
-    public boolean isFinished() {
-        return MathUtil.compareSetpoint(Arm.getInstance().getPosition(), ref, RobotMap.Arm.MAX_ERROR);
-    }
+//     public boolean isFinished() {
+//         return MathUtil.compareSetpoint(Arm.getInstance().getPosition(), ref, RobotMap.Arm.MAX_ERROR);
+//     }
 
-    // public void end(boolean interrupted) {
-    //     Pivot.getInstance().setPercentOutput(0);
-    // }
+//     // public void end(boolean interrupted) {
+//     //     Pivot.getInstance().setPercentOutput(0);
+//     // }
 
-}
+// }
